62ccf82dcc53d2f9d74039c1c0406330b1e0af05
Igor Turcan <igortsurkan99@gmail.com>
2023-04-30 15:45:20 +0300

    Initiated

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Contacts App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const ContactsPage(),
+    );
+  }
+}
+
+class ContactsPage extends StatelessWidget {
+  const ContactsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Contacts page'),
+      ),
+      body: const Center(
+        child: Text('Contacts page here'),
+      ),
+    );
+  }
+}
