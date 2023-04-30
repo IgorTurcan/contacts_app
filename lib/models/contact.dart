c66152ed95740188143e36ed42f75e89cf8e68fc
Igor Turcan <igortsurkan99@gmail.com>
2023-04-30 16:06:19 +0300

    Contact model added

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -1,1 +1,25 @@
-class Contact {}
+import 'package:objectbox/objectbox.dart';
+
+@Entity()
+class Contact {
+  @Id()
+  String contactID;
+  String? firstName;
+  String? lastName;
+  String? streetAddress1;
+  String? streetAddress2;
+  String? city;
+  String? state;
+  String? zipCode;
+
+  Contact({
+    required this.contactID,
+    this.firstName,
+    this.lastName,
+    this.streetAddress1,
+    this.streetAddress2,
+    this.city,
+    this.state,
+    this.zipCode,
+  }) : assert(firstName != null || lastName != null, 'Contact must have at least on name!');
+}
